24d581f8b2d5a59d0f624d0d28dc9f474e91f6a4
Mateusz <mmoczydlowski4@gmail.com>
2020-08-11 16:14:03 +0200

    Add support for mobile devices
    Add responsive design, little fixes to the sizes and overlook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,17 @@
 let scrolled = false;
 window.onscroll = function(ev) {
-    if (!scrolled)
+    if (document.body.scrollTop === 0)
+        document.getElementById("header").classList.remove("header--active");
+    else
         document.getElementById("header").classList.add("header--active");
-        scrolled = true;
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
